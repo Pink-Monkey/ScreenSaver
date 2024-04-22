f40fb419ce3be96aaec5900e150bf605f37bcccb
Pink-Monkey <148002073+Pink-Monkey@users.noreply.github.com>
2024-04-21 20:26:17 -0400

    Update base.py

--- base.py
+++ base.py
@@ -1,31 +1,60 @@
+# -*- coding: utf-8 -*-
+
 import pyautogui
 import time
+import random
+import sys
+
+pyautogui.FAILSAFE = False
 
 
+def switch_screens() -> None:
+    
+    #Switches the active screen using Alt + Tab
+    #a random number of times.
+    
+    max_switches = random.randint(1, 5)
+    pyautogui.keyDown('alt') 
+    
+    for _ in range(1, max_switches):
+        pyautogui.press('tab')     
+     
+    pyautogui.keyUp('alt')   
 
 
-time_str = input("How many minutes would you like to run this command?")
+def wiggle_mouse() -> None:
 
-run_str = "0"
-run_base = int(run_str)
+    max_wiggles = random.randint(4, 9)
+    
+    for _ in range(1, max_wiggles):
+        coords = get_random_coords()
+        pyautogui.moveTo(
+            x=coords[0], 
+            y=coords[1],
+            duration=5
+        )
+        time.sleep(10)
+    
 
-x = True
+def get_random_coords() -> []:
 
-print("You have chosen " + time_str + " minutes.")
-print("Rerun this code if you have typed incorrectly.")
-
-time_int = int(time_str)
-
-time_var = time_int * 60
-
-print(time_var)
-
-while x == True:
-
-    run_var = run_base + 1
-    run_base = run_var
-    print(run_var)
-    time.sleep(1)
+    
+    screen = pyautogui.size()
+    width = screen[0]
+    height = screen[1]
+    
+    return [
+        random.randint(100, width - 200),
+        random.randint(100, height - 200)
+    ]
 
 
-pyautogui.dragRel(0, 100, duration = 0.5)
+if __name__ == "__main__":
+    print('Press Ctrl-C to quit.')
+    try:
+        while True:
+            switch_screens()
+            wiggle_mouse()
+            sys.stdout.flush()
+    except KeyboardInterrupt:
+        print("\n")
